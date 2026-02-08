e526a80dae27f81809e563e8748c524b3a8a05d9
$GIT_USERNAME <$GIT_EMAIL>
2026-02-08 19:41:00 +0000

    chore: update __about__.py metadata

--- src/django_grep/__about__.py
+++ src/django_grep/__about__.py
@@ -1,1 +1,18 @@
+"""
+@author Mahmoud Ezzat
+@requires Python 3.11 or later
+
+Copyright (c) 2024
+All rights reserved.
+"""
+
 __version__ = "1.0.3"
+__version_info__ = tuple(
+    [
+        int(num) if num.isdigit() else num
+        for num in __version__.replace("-", ".", 1).split(".")
+    ]
+)
+
+__name__ = "django-grep"
+__description__ = "Generic Reusable Enhanced Pipelines for Django and Wagtail"
